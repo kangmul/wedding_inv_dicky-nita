fc4cdfc624c170cb760f01fc7e9f5f0465f4f7a0
LAPTOP-8EUNS5KJ\cbn <muluk.dharmayana@gmail.com>
2022-05-04 22:08:49 +0700

    perbaikan cover size full

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ let btnBukaUndangan = document.getElementById('bukaundangan');
 btnBukaUndangan.addEventListener('click', function() {
   let displayshow = document.getElementById('content');
   displayshow.classList.remove('d-none')
-  let cover = document.getElementById('cover')
+  let cover = document.getElementById('img-cover')
   cover.classList.add('d-none');
 })
